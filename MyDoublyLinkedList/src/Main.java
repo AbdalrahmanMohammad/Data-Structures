ca4c779b60482f4bd167ad04f340cfdf20af9955
Abdalrahman-22 <abdalrahman.s.mohammad@gmail.com>
2024-08-26 12:57:02 +0300

    add adding functions

--- MyDoublyLinkedList/src/Main.java
+++ MyDoublyLinkedList/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
 public static void main(String[] args) {
-    System.out.println("lovkjke");
+    MyDoublyLinkedList<Integer> list = new MyDoublyLinkedList<>();
+
+    // Test add(T val)
+    list.add(10);
+    list.add(20);
+    list.add(30);
+    list.print(); // Expected output: { 10 | 20 | 30 } 3
+
+    // Test addFirst(T val)
+    list.addFirst(5);
+    list.print(); // Expected output: { 5 | 10 | 20 | 30 } 4
+
+    // Test addAtIndex(int k, T val)
+    list.addAtIndex(2, 15);
+    list.print(); // Expected output: { 5 | 10 | 15 | 20 | 30 } 5
+
+    // Test adding at the end using addAtIndex
+    list.addAtIndex(5, 35);
+    list.print(); // Expected output: { 5 | 10 | 15 | 20 | 30 | 35 } 6
+    list.printReversed();
+
+
+    // Test adding at index 0 using addAtIndex
+    list.addAtIndex(0, 0);
+    list.print(); // Expected output: { 0 | 5 | 10 | 15 | 20 | 30 | 35 } 7
+
+    // Test out of range index for addAtIndex
+    list.addAtIndex(10, 50); // Expected output: "that is out of range"
+    list.print(); // Should remain unchanged: { 0 | 5 | 10 | 15 | 20 | 30 | 35 } 7
+    list.printReversed();
 }
 }
--- MyDoublyLinkedList/src/MyDoublyLinkedList.java
+++ MyDoublyLinkedList/src/MyDoublyLinkedList.java
@@ -21,10 +21,74 @@ public class MyDoublyLinkedList<T> {
             tail = head;
         } else {
             tail.setNext(new Node<T>(val));
-            tail.getNext().setPrevious(tail);;
+            tail.getNext().setPrevious(tail);
+            ;
             tail = tail.getNext();
         }
         size++;
+    }
+
+    public void addFirst(T val) {
+        Node<T> temp = new Node<>(val);
+        if (head == null) {
+            head = temp;
+            tail = temp;
+        } else {
+            temp.setNext(head);
+            head.setPrevious(temp);
+            head = temp;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int k, T val) {
+        if (k > size || k < 0) {
+            System.out.println("that is out of range");
+        } else if (k == size) {
+            add(val);
+        } else if (k == 0) {
+            addFirst(val);
+        } else {
+            Node<T> temp = head, newNode = new Node<>(val);
+            for (int i = 0; i < k - 1; i++) {
+                temp = temp.getNext();
+            }
+            newNode.setNext(temp.getNext());
+            temp.setNext(newNode);
+            newNode.getNext().setPrevious(newNode);
+            newNode.setPrevious(temp);
+            size++;
+        }
+    }
+
+    public void print() {
+        StringBuilder ans = new StringBuilder("{ ");
+        Node<T> temp = head;
+        while (temp != null) {
+            ans.append(temp.getValue()).append(" | ");
+            temp = temp.getNext();
+        }
+        if (ans.length() > 2) {
+            ans.setLength(ans.length() - 2); // Remove the last " | "
+        }
+        ans.append(" } ");
+        ans.append(size);
+        System.out.println(ans.toString());
+    }
+
+    public void printReversed() {
+        StringBuilder ans = new StringBuilder("{ ");
+        Node<T> temp = tail;
+        while (temp != null) {
+            ans.append(temp.getValue()).append(" | ");
+            temp = temp.getPrevious();
+        }
+        if (ans.length() > 2) {
+            ans.setLength(ans.length() - 2); // Remove the last " | "
+        }
+        ans.append(" } ");
+        ans.append(size);
+        System.out.println(ans.toString());
     }
 
 }
