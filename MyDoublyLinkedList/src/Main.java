f0c852c0c288820dabd51d37293422c04d5db3a3
Abdalrahman-22 <abdalrahman.s.mohammad@gmail.com>
2024-08-30 00:30:44 +0300

    implement add functions

--- MyDoublyLinkedList/src/Main.java
+++ MyDoublyLinkedList/src/Main.java
@@ -2,6 +2,44 @@ public class Main {
     public static void main(String[] args) {
         MyCircularLinkedList<Integer> list = new MyCircularLinkedList<>();
 
+        // Test: Adding elements to the list
+        System.out.println("Adding elements:");
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.print(); // Expected output: { 10 | 20 | 30 } 3
+
+        // Test: Adding an element at the beginning
+        System.out.println("\nAdding element at the beginning:");
+        list.addFirst(5);
+        list.print(); // Expected output: { 5 | 10 | 20 | 30 } 4
+
+        // Test: Adding an element at a specific index
+        System.out.println("\nAdding element at index 2:");
+        list.addAtIndex(2, 15);
+        list.print(); // Expected output: { 5 | 10 | 15 | 20 | 30 } 5
+
+        // Test: Adding an element at the end (same as adding normally)
+        System.out.println("\nAdding element at the end:");
+        list.addAtIndex(list.size, 35);
+        list.print(); // Expected output: { 5 | 10 | 15 | 20 | 30 | 35 } 6
+
+        // Test: Attempting to add at an invalid index
+        System.out.println("\nAdding element at invalid index:");
+        list.addAtIndex(10, 50); // Expected output: that is out of range
+        list.print(); // Expected output: { 5 | 10 | 15 | 20 | 30 | 35 } 6
+
+        // Test: Creating a list with a single initial value
+        System.out.println("\nCreating a new list with an initial value:");
+        MyCircularLinkedList<String> stringList = new MyCircularLinkedList<>("Hello");
+        stringList.print(); // Expected output: { Hello } 1
+
+        // Test: Adding more elements to the new list
+        stringList.add("World");
+        stringList.print(); // Expected output: { Hello } 1
+
+        stringList.add("!");
+        stringList.print(); // Expected output: { Hello | World | ! } 3
         
     }
 }
--- MyDoublyLinkedList/src/MyCircularLinkedList.java
+++ MyDoublyLinkedList/src/MyCircularLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.Iterator;
 
-public class MyCircularLinkedList<T>  {
+public class MyCircularLinkedList<T> {
     int size;
     Node<T> head;
     Node<T> tail;
@@ -13,247 +13,253 @@ public class MyCircularLinkedList<T>  {
 
     MyCircularLinkedList(T val) {
         head = new Node<T>(val);
+        head.setNext(head);
         tail = head;
         size++;
     }
 
-    // public void add(T val) {
-    //     if (head == null) {
-    //         head = new Node<T>(val);
-    //         tail = head;
-    //     } else {
-    //         tail.setNext(new Node<T>(val));
-    //         tail.getNext().setPrevious(tail);
-    //         ;
-    //         tail = tail.getNext();
-    //     }
-    //     size++;
-    // }
+    public void add(T val) {
+        if (head == null) {
+            head = new Node<T>(val);
+            head.setNext(head);
+            tail = head;
+        } else {
+            tail.setNext(new Node<T>(val));
+            tail = tail.getNext();
+            tail.setNext(head);
+        }
+        size++;
+    }
 
-    // public void addFirst(T val) {
-    //     Node<T> temp = new Node<>(val);
-    //     if (head == null) {
-    //         head = temp;
-    //         tail = temp;
-    //     } else {
-    //         temp.setNext(head);
-    //         head.setPrevious(temp);
-    //         head = temp;
-    //     }
-    //     size++;
-    // }
+    public void addFirst(T val) {
+        Node<T> temp = new Node<>(val);
+        if (head == null) {
+            head = temp;
+            head.setNext(head);
+            tail = temp;
+        } else {
+            temp.setNext(head);
+            head = temp;
+            tail.setNext(head);
+        }
+        size++;
+    }
 
-    // public void addAtIndex(int k, T val) {
-    //     if (k > size || k < 0) {
-    //         System.out.println("that is out of range");
-    //     } else if (k == size) {
-    //         add(val);
-    //     } else if (k == 0) {
-    //         addFirst(val);
-    //     } else {
-    //         Node<T> temp = head, newNode = new Node<>(val);
-    //         for (int i = 0; i < k - 1; i++) {
-    //             temp = temp.getNext();
-    //         }
-    //         newNode.setNext(temp.getNext());
-    //         temp.setNext(newNode);
-    //         newNode.getNext().setPrevious(newNode);
-    //         newNode.setPrevious(temp);
-    //         size++;
-    //     }
-    // }
+    public void addAtIndex(int k, T val) {
+        if (k > size || k < 0) {
+            System.out.println("that is out of range");
+        } else if (k == size) {
+            add(val);
+        } else if (k == 0) {
+            addFirst(val);
+        } else {
+            Node<T> temp = head, newNode = new Node<>(val);
+            for (int i = 0; i < k - 1; i++) {
+                temp = temp.getNext();
+            }
+            newNode.setNext(temp.getNext());
+            temp.setNext(newNode);
+            size++;
+        }
+    }
 
-    // public void print() {
-    //     StringBuilder ans = new StringBuilder("{ ");
-    //     Node<T> temp = head;
-    //     while (temp != null) {
-    //         ans.append(temp.getValue()).append(" | ");
-    //         temp = temp.getNext();
-    //     }
-    //     if (ans.length() > 2) {
-    //         ans.setLength(ans.length() - 2); // Remove the last " | "
-    //     }
-    //     ans.append(" } ");
-    //     ans.append(size);
-    //     System.out.println(ans.toString());
-    // }
+    public void print() {
+        StringBuilder ans = new StringBuilder("{ ");
+        Node<T> temp = head;
+        if (temp == null) {
+            System.out.println("{}");
+            return;
+        }
+
+        do {
+            ans.append(temp.getValue()).append(" | ");
+            temp = temp.getNext();
+        } while (temp != head);
+        
+        if (ans.length() > 2) {
+            ans.setLength(ans.length() - 3); // Remove the last " | "
+        }
+        ans.append(" } ");
+        ans.append(size);
+        System.out.println(ans.toString());
+    }
 
     // public void printReversed() {
-    //     StringBuilder ans = new StringBuilder("{ ");
-    //     Node<T> temp = tail;
-    //     while (temp != null) {
-    //         ans.append(temp.getValue()).append(" | ");
-    //         temp = temp.getPrevious();
-    //     }
-    //     if (ans.length() > 2) {
-    //         ans.setLength(ans.length() - 2); // Remove the last " | "
-    //     }
-    //     ans.append(" } ");
-    //     ans.append(size);
-    //     System.out.println(ans.toString());
+    // StringBuilder ans = new StringBuilder("{ ");
+    // Node<T> temp = tail;
+    // while (temp != null) {
+    // ans.append(temp.getValue()).append(" | ");
+    // temp = temp.getPrevious();
+    // }
+    // if (ans.length() > 2) {
+    // ans.setLength(ans.length() - 2); // Remove the last " | "
+    // }
+    // ans.append(" } ");
+    // ans.append(size);
+    // System.out.println(ans.toString());
     // }
 
     // public int getSize() {
-    //     return size;
+    // return size;
     // }
 
     // public T getFirst() {
-    //     return head.getValue();
+    // return head.getValue();
     // }
 
     // public T getLast() {
-    //     return tail.getValue();
+    // return tail.getValue();
     // }
 
     // public T getAtIndex(int k) {
-    //     if (k >= size || k < 0) {
-    //         System.out.println("that is out of range");
-    //         return null;
-    //     } else if (k == 0) {
-    //         return getFirst();
-    //     } else if (k == size - 1) {
-    //         return getLast();
-    //     } else {
-    //         Node<T> temp = head;
-    //         for (int i = 0; i < k; i++) {
-    //             temp = temp.getNext();
-    //         }
-    //         return temp.getValue();
-    //     }
+    // if (k >= size || k < 0) {
+    // System.out.println("that is out of range");
+    // return null;
+    // } else if (k == 0) {
+    // return getFirst();
+    // } else if (k == size - 1) {
+    // return getLast();
+    // } else {
+    // Node<T> temp = head;
+    // for (int i = 0; i < k; i++) {
+    // temp = temp.getNext();
+    // }
+    // return temp.getValue();
+    // }
     // }
 
     // public boolean contains(T val) {
-    //     Node<T> temp = head;
-    //     while (temp != null) {
-    //         if (temp.getValue().equals(val))
-    //             return true;
-    //         temp = temp.getNext();
-    //     }
-    //     return false;
+    // Node<T> temp = head;
+    // while (temp != null) {
+    // if (temp.getValue().equals(val))
+    // return true;
+    // temp = temp.getNext();
+    // }
+    // return false;
     // }
 
     // public boolean isEmpty() {
-    //     return size == 0;
+    // return size == 0;
     // }
 
     // public void clear() {
-    //     size = 0;
-    //     head = null;
-    //     tail = null;
+    // size = 0;
+    // head = null;
+    // tail = null;
     // }
 
     // public void removeFirst() {
-    //     if (size <= 1) {
-    //         clear();
-    //         return;
-    //     }
-    //     head = head.getNext();
-    //     head.setPrevious(null);
-    //     size--;
+    // if (size <= 1) {
+    // clear();
+    // return;
+    // }
+    // head = head.getNext();
+    // head.setPrevious(null);
+    // size--;
 
     // }
 
     // public void removeLast() {
-    //     if (size <= 1) {
-    //         clear();
-    //         return;
-    //     }
-    //     tail = tail.getPrevious();
-    //     tail.setNext(null);
-    //     size--;
+    // if (size <= 1) {
+    // clear();
+    // return;
+    // }
+    // tail = tail.getPrevious();
+    // tail.setNext(null);
+    // size--;
     // }
 
     // public void removeAtIndex(int k) {
-    //     if (k < 0 || k >= size) {
-    //         System.out.println("out of bound");
-    //     } else if (k == 0) {
-    //         removeFirst();
-    //     } else if (k == size - 1) {
-    //         removeLast();
-    //     } else {
-    //         Node<T> temp = head;
-    //         for (int i = 0; i < k - 1; i++) {
-    //             temp = temp.getNext();
-    //         }
-    //         temp.setNext(temp.getNext().getNext());
-    //         temp.getNext().setPrevious(temp);
-    //         size--;
-    //     }
+    // if (k < 0 || k >= size) {
+    // System.out.println("out of bound");
+    // } else if (k == 0) {
+    // removeFirst();
+    // } else if (k == size - 1) {
+    // removeLast();
+    // } else {
+    // Node<T> temp = head;
+    // for (int i = 0; i < k - 1; i++) {
+    // temp = temp.getNext();
+    // }
+    // temp.setNext(temp.getNext().getNext());
+    // temp.getNext().setPrevious(temp);
+    // size--;
+    // }
     // }
 
     // @Override
     // public Iterator<T> iterator() {
-    //     return new myIterator();
+    // return new myIterator();
     // }
 
     // private class myIterator implements Iterator<T> {
-    //     Node<T> temp = head;
+    // Node<T> temp = head;
 
-    //     @Override
-    //     public boolean hasNext() {
-    //         return temp != null;
-    //     }
+    // @Override
+    // public boolean hasNext() {
+    // return temp != null;
+    // }
 
-    //     @Override
-    //     public T next() {
-    //         T x = temp.getValue();
-    //         temp = temp.getNext();
-    //         return x;
-    //     }
+    // @Override
+    // public T next() {
+    // T x = temp.getValue();
+    // temp = temp.getNext();
+    // return x;
+    // }
 
     // }
 
     // public void reverse() {
-    //     Node<T> temp = head, headd = head, taill = tail;
-    //     while (temp != null) {
-    //         Node<T> aa = temp.getNext();
-    //         temp.setNext(temp.getPrevious());
-    //         temp.setPrevious(aa);
-    //         temp = temp.getPrevious();
-    //     }
-    //     temp = headd;
-    //     head = taill;
-    //     tail = temp;
+    // Node<T> temp = head, headd = head, taill = tail;
+    // while (temp != null) {
+    // Node<T> aa = temp.getNext();
+    // temp.setNext(temp.getPrevious());
+    // temp.setPrevious(aa);
+    // temp = temp.getPrevious();
+    // }
+    // temp = headd;
+    // head = taill;
+    // tail = temp;
     // }
 
     // public void reverse2() {
-    //     Node<T> headd = head, taill = tail;
+    // Node<T> headd = head, taill = tail;
 
-    //     Node<T> temp, prev = null;
+    // Node<T> temp, prev = null;
 
-    //     while (head != null) {
-    //         temp = head.getNext();
-    //         head.setNext(prev);
-    //         head.setPrevious(temp);
-    //         prev = head;
-    //         head = temp;
-    //     }
+    // while (head != null) {
+    // temp = head.getNext();
+    // head.setNext(prev);
+    // head.setPrevious(temp);
+    // prev = head;
+    // head = temp;
+    // }
 
-    //     temp = headd;
-    //     head = taill;
-    //     tail = temp;
+    // temp = headd;
+    // head = taill;
+    // tail = temp;
     // }
 
     // public void reverse3() {
-    //     Node<T> temp = head, headd = head, taill = tail;
-    //     reverseHelper(temp);
-    //     temp = headd;
-    //     head = taill;
-    //     tail = temp;
-    //     tail.setNext(null);
-    //     head.setPrevious(null);
+    // Node<T> temp = head, headd = head, taill = tail;
+    // reverseHelper(temp);
+    // temp = headd;
+    // head = taill;
+    // tail = temp;
+    // tail.setNext(null);
+    // head.setPrevious(null);
     // }
 
     // public Node<T> reverseHelper(Node<T> head) {
-    //     if (head == null || head.getNext() == null) {
-    //         return head;
-    //     }
+    // if (head == null || head.getNext() == null) {
+    // return head;
+    // }
 
-    //     Node<T> next=head.getNext();
-    //     Node<T> rest = reverseHelper(head.getNext());
-    //     next.setNext(head);
-    //     head.setPrevious(next);
-    //     return rest;
+    // Node<T> next=head.getNext();
+    // Node<T> rest = reverseHelper(head.getNext());
+    // next.setNext(head);
+    // head.setPrevious(next);
+    // return rest;
 
     // }
 
