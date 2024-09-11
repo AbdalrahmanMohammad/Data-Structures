0d83e98aa6ec3e9e3ce6c323031743da14ae9be1
Abdalrahman-22 <abdalrahman.s.mohammad@gmail.com>
2024-09-11 09:39:54 +0300

    nothing

---
+++ MyCircularLinkedList/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        MyCircularLinkedList<Integer> list = new MyCircularLinkedList<>();
+
+        // Test isEmpty() on a new list
+        System.out.println("List is empty: " + list.isEmpty()); // Expected: true
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);  
+        list.add(50);
+        System.out.print("List after adding elements (10, 20, 30): ");
+        list.print(); // Expected: { 10 | 20 | 30 } 3
+
+        list.rotate(6);
+        list.print();
+
+    }
+}
